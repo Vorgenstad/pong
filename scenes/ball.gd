50acc80c89121254910d03055ffed2f1188e90fa
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2023-11-11 18:02:33 +0100

    Add basic ball movement

---
+++ scenes/ball.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var speed = 400
+
+var x_direction
+var y_direction
+
+func _ready():
+	x_direction = pick_random_option(2 , -2)
+	y_direction = pick_random_option(1 , -1)
+
+func _process(delta):	
+	var velocity = Vector2(x_direction, y_direction).normalized() * speed
+
+	position += velocity * delta
+
+func pick_random_option(a, b):
+	return a if randi() % 2 else -b
