05c9721573bbbd395b381b30f88b65e5b3298ddb
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2023-11-19 18:56:50 +0100

    Added ball start timer

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -4,25 +4,29 @@ extends Area2D
 
 var x_direction
 var y_direction
-var stopped = false
+var moving = false
 
 const initial_position = Vector2(640, 360)
 
 func _ready():
-	x_direction = pick_random_option(2 , -2)
-	y_direction = pick_random_option(1 , -1)
+	initialize()
 
 func _process(delta):	
-	if not stopped:
+	if moving:
 		var velocity = Vector2(x_direction, y_direction).normalized() * speed
 		position += velocity * delta
 
 func pick_random_option(a, b):
 	return a if randi() % 2 else b
 
-func reset():
+func initialize():
+	x_direction = pick_random_option(2 , -2)
+	y_direction = pick_random_option(1 , -1)
 	position = initial_position
-	stopped = true
+	moving = false
+
+func start_moving():
+	moving = true
 
 func _on_area_entered(area):
 	if area.get_collision_layer() == 1:
--- scenes/main.gd
+++ scenes/main.gd
@@ -4,9 +4,14 @@ var player_1_score = 0
 var player_2_score = 0
 
 func _on_goal_area_entered(_area: Area2D, player_that_scored: int):
-	$Ball.reset()
+	$Ball.initialize()
 
 	if player_that_scored == 1:
 		player_1_score += 1
 	else:
 		player_2_score += 1
+	
+	$StartTimer.start()
+
+func _on_start_timer_timeout():
+	$Ball.start_moving()
