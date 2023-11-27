a518e279d80705f378d55dc75874609d354d4438
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2023-11-27 19:32:01 +0100

    Renamed Main scene to Game

--- scenes/game.gd
+++ scenes/game.gd
@@ -5,7 +5,7 @@ var player_2_score = 0
 
 func _on_goal_area_entered(_area: Area2D, player_that_scored: int):
 	$Ball.initialize()
-
+    
 	if player_that_scored == 1:
 		player_1_score += 1
 		$UI.update_score(1, player_1_score)
