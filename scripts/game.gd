b1409e081373a44ee9767ca48b3537949a30baec
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2024-08-31 16:21:59 +0200

    Reworked how players are referenced

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,25 +2,26 @@ extends Node2D
 
 @export var winning_score: int
 
-var player_1_score := 0
-var player_2_score := 0
+var scores := {
+	Constants.Player.LEFT: 0,
+	Constants.Player.RIGHT: 0,
+}
 
 @onready var ball: Ball = $Ball
 @onready var ui: Ui = $UI
 @onready var start_timer: Timer = $StartTimer
 
-func _on_goal_area_entered(_area: Area2D, player_that_scored: int) -> void:
+func _on_goal_area_entered(_area: Area2D, player_that_scored: Constants.Player) -> void:
 	ball.initialize()
 	
-	if player_that_scored == 1:
-		player_1_score += 1
-		ui.update_score(1, player_1_score)
-	else:
-		player_2_score += 1
-		ui.update_score(2, player_2_score)
-	
-	if player_1_score == winning_score || player_2_score == winning_score:
-		ui.display_game_over(1 if player_1_score == winning_score else 2)
+	var new_score: int = scores[player_that_scored] + 1
+
+	scores[player_that_scored] = new_score
+
+	ui.update_score(player_that_scored, new_score)
+
+	if new_score == winning_score:
+		ui.display_game_over(player_that_scored)
 	else:
 		start_timer.start()
 
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -5,8 +5,8 @@ extends CanvasLayer
 
 @onready var info_label: Label = $InfoLabel
 @onready var start_text_timer: Timer = $StartTextTimer
-@onready var player_1_score_label: Label = $Player1ScoreLabel
-@onready var player_2_score_label: Label = $Player2ScoreLabel
+@onready var left_score_label: Label = $LeftScoreLabel
+@onready var right_score_label: Label = $RightScoreLabel
 
 func _process(_delta: float) -> void:
 	if timer.time_left != 0:
@@ -16,14 +16,25 @@ func display_start_text() -> void:
 	info_label.text = "Go!"
 	start_text_timer.start()
 
-func update_score(player: int, score: int) -> void:
-	if (player == 1):
-		player_1_score_label.text = str(score)
-	else:
-		player_2_score_label.text = str(score)
+func update_score(player: Constants.Player, score: int) -> void:
+	match player:
+		Constants.Player.LEFT:
+			left_score_label.text = str(score)
+		Constants.Player.RIGHT:
+			right_score_label.text = str(score)
 
-func display_game_over(winning_player: int) -> void:
-	info_label.text = "Player %s wins!" % winning_player
+func display_game_over(winning_player: Constants.Player) -> void:
+	info_label.text = "%s player wins!" % format_player_name(winning_player)
+
+func format_player_name(player: Constants.Player) -> String:
+	match player:
+		Constants.Player.LEFT:
+			return "Left"
+		Constants.Player.RIGHT:
+			return "Right"
+		_:
+			printerr("Unexpected player" + str(player))
+			return ""
 
 func _on_start_text_timer_timeout() -> void:
 	info_label.text = ""
