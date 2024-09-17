0711af44b84f1552b3e72a5fe6105cbe3d415a95
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2024-09-17 09:49:24 +0200

    Added basic AI controller for palettes

--- scripts/ai_controller.gd
+++ scripts/ai_controller.gd
@@ -1,26 +1,24 @@
-class_name Controller
+class_name AIController
 extends Node
 
 @export var palette: Palette
 
 const _speed = 300
 
-var _up_action: String
-var _down_action: String
-
 var _upper_limit: float
 var _lower_limit: float
 
+@onready var _ball: Ball = get_node("../%Ball")
+
 func _ready() -> void:
 	_set_movement_limits()
-	_set_actions()
 
 func _process(delta: float) -> void:
 	var direction := 0
 	
-	if Input.is_action_pressed(_up_action):
+	if _ball.position.y < palette.position.y:
 		direction = -1
-	if Input.is_action_pressed(_down_action):
+	if _ball.position.y > palette.position.y:
 		direction = 1
 	
 	if direction != 0:
@@ -35,12 +33,3 @@ func _set_movement_limits() -> void:
 	var screen_height := get_viewport().get_visible_rect().size.y
 
 	_lower_limit = screen_height - height / 2.0
-
-func _set_actions() -> void:
-	match palette.player:
-		Constants.Player.LEFT:
-			_up_action = "left_player_up"
-			_down_action = "left_player_down"
-		Constants.Player.RIGHT:
-			_up_action = "right_player_up"
-			_down_action = "right_player_down"
