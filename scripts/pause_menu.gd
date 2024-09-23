bf3ac81cb168c5f70e270b09d5b90ec9d61d53a7
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2024-09-23 22:39:26 +0200

    Added pause menu

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@onready var _menu_elements: CanvasLayer = %MenuElements
+@onready var _tree := get_tree()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		_toggle_pause()
+
+func _toggle_pause() -> void:
+	_tree.paused = !_tree.paused
+	_menu_elements.visible = !_menu_elements.visible
+
+func _on_resume_button_pressed() -> void:
+	_toggle_pause()
