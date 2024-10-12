e31fb1e339254e3a74d4d551c3571b1ac18df1ea
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2024-10-12 15:24:23 +0200

    Added an info label on the menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,16 +7,22 @@ var _second_player_type: Constants.ControllerType
 @onready var _play_button: Button = %PlayButton
 @onready var _score_input_container: Container = %ScoreInputContainer
 @onready var _player_input_container: Container = %PlayerInputContainer
+@onready var _info_label: Label = %InfoLabel
 
 func _on_play_button_pressed() -> void:
 	_play_button.visible = false
 	_player_input_container.visible = true
+
+	_info_label.text = "Select mode"
+	_info_label.visible = true
 
 func _on_players_selected(second_player_type: Constants.ControllerType) -> void:
 	_second_player_type = second_player_type
 
 	_player_input_container.visible = false
 	_score_input_container.visible = true
+
+	_info_label.text = "Select winning score"
 
 func _on_winning_score_selected(winning_score: int) -> void:
 	start_actioned.emit(winning_score, _second_player_type)
